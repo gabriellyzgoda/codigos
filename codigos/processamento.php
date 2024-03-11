6a3174ee9c956e8d162bcbd34a3000643058c06b
Gabrielly Zgoda <142603509+gabriellyzgoda@users.noreply.github.com>
2024-03-10 21:01:52 -0300

    atualização com fundo

---
+++ codigos/processamento.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Processamento</title>
+</head>
+<style>
+    body{
+        margin: 0;
+        padding: 0;
+    }
+    .fundo{
+        width: 100%;
+        height:641px;
+        background-image: url("fut.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+    .fut{
+    background-color:white;
+    opacity: 0.7;
+    border:solid 2px black;
+    border-radius:10px;
+    width:500px;
+    height:50%; 
+    }
+</style>
+<body>
+<div class="fundo">
+<center>
+    <br>
+    <br>
+<br>
+<br>
+<br>
+<br>
+<div class="fut">
+    
+        <?php
+        ?>
+<a href= "futebol.php">Voltar</a>
+</div>
+</div>
+</center>
+</body>
+</html>
